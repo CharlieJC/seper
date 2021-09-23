d5eeb8cfad6e9dc9a2da8e3af4c4a474251b0853
David <davidbrettandersonnz@gmail.com>
2021-09-23 14:09:14 +1200

    removed comments

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -3,8 +3,6 @@ import React from 'react';
 export function Dropdown({
     column: { filterValue, setFilter, preFilteredRows, id },
   }) {
-    // Calculate the options for filtering
-    // using the preFilteredRows
     const options = React.useMemo(() => {
       const options = new Set()
       preFilteredRows.forEach(row => {
@@ -13,7 +11,6 @@ export function Dropdown({
       return [...options.values()]
     }, [id, preFilteredRows])
   
-    // Render a multi-select box
     return (
       <select
         value={filterValue}
@@ -29,22 +26,4 @@ export function Dropdown({
         ))}
       </select>
     )
-// export const Dropdown = ({ column })=> {
-//  const { setFilter, preFilteredRows } = column
-
-//  const optionItems = SEPractices.map((SEPractice) =>
-//  <option key={SEPractice.practice}>{SEPractice.practice}</option>
-// );
-
-// console.log(optionItems);
-
-//  return (
-//     <select
-//         value={optionItems}
-//         onChange={e => {setFilter(e.target.value)}}
-//     >
-//     <option value="">All</option>
-//        {optionItems}
-//     </select>
-//     )
 }
