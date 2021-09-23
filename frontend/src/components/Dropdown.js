b6f79084533180606f6e5bcc2a24ff415e6b7f26
David <davidbrettandersonnz@gmail.com>
2021-09-23 13:44:01 +1200

    Issue Resolved Sorts correctly now

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,29 +1,50 @@
 import React from 'react';
-import SEPractices from './SEPractices';
 
-
-export const Dropdown = ({ column })=> {
- const { filterValue, setFilter } = column
-
- const optionItems = SEPractices.map((SEPractice) =>
- <option key={SEPractice.practice}>{SEPractice.practice}</option>
-);
-
-console.log(optionItems);
-
- return (
-    <select
-        value={optionItems}
-        onChange={e => {setFilter(e.target.value)}}
-    >
-    <option value="">All</option>
-       {optionItems}
-    </select>
+export function Dropdown({
+    column: { filterValue, setFilter, preFilteredRows, id },
+  }) {
+    // Calculate the options for filtering
+    // using the preFilteredRows
+    const options = React.useMemo(() => {
+      const options = new Set()
+      preFilteredRows.forEach(row => {
+        options.add(row.values[id])
+      })
+      return [...options.values()]
+    }, [id, preFilteredRows])
+  
+    // Render a multi-select box
+    return (
+      <select
+        value={filterValue}
+        onChange={e => {
+          setFilter(e.target.value || undefined)
+        }}
+      >
+        <option value="">All</option>
+        {options.map((option, i) => (
+          <option key={i} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     )
-    // <div>
-    //     <select>
-    //         {optionItems}
-    //     <input value={filterValue || ''} onChange={(e) => setFilter(e.target.value)}/>
-    //     </select>
-    // </div>
+// export const Dropdown = ({ column })=> {
+//  const { setFilter, preFilteredRows } = column
+
+//  const optionItems = SEPractices.map((SEPractice) =>
+//  <option key={SEPractice.practice}>{SEPractice.practice}</option>
+// );
+
+// console.log(optionItems);
+
+//  return (
+//     <select
+//         value={optionItems}
+//         onChange={e => {setFilter(e.target.value)}}
+//     >
+//     <option value="">All</option>
+//        {optionItems}
+//     </select>
+//     )
 }
